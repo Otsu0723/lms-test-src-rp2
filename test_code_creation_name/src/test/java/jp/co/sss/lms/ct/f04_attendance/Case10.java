3ed47f87d00496a1f774320e062664f738157515
otsuka <otsuka@LAPTOP-UV920EUR>
2025-10-24 13:20:35 +0900

    fix: Case10.java テストケース作成
    test04()メソッドまでの修正

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,28 +39,65 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement loginId = webDriver.findElement(By.name("loginId"));
+		WebElement password = webDriver.findElement(By.name("password"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA03");
+		password.clear();
+		password.sendKeys("StudentAA0303");
+
+		WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+		loginButton.click();
+
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// ナビゲーションバーから勤怠管理画面に遷移
+		WebElement attendance = webDriver.findElement(By.linkText("勤怠"));
+		attendance.click();
+
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		assertEquals("http://localhost:8080/lms/attendance/detail", webDriver.getCurrentUrl());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		// 出勤ボタンを押下
+		WebElement punchIn = webDriver.findElement(By.name("punchIn"));
+		punchIn.click();
+
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		WebElement comp = webDriver.findElement(By.className("alert-dismissible"));
+		System.out.println(comp.getText());
+		assertEquals("×\n勤怠情報の登録が完了しました。", comp.getText());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
